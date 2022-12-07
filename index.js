71a6966e4685bca19c991437efbee6106869008c
PapitoSpence <PapitoSpence>
2022-12-07 16:05:03 +0300

    commit tree 8bd2189e6907d6277a1dddccc4710997e1c439fe

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
 }
+const btn = document.getElementById('button');
+function clickAlert() {
+    alert('I was clicked!');
+  }
+  if (btn) {
+  btn.addEventListener('click', () => {
+    console.log('btn clicked');
+  });
+}
+
+function addingEventListener() {
+    btn.addEventListener("click", clickAlert);
+}
+
+addingEventListener("click", clickAlert);
